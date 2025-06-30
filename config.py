845f0c910eba16df354b22171b3271d6b21aec99
naruto1427 <pritnaruto@gmail.com>
2025-06-30 10:48:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","rename")     
-    DB_URL  = os.environ.get("DB_URL","")
+    DB_URL  = os.environ.get("DB_URL","-1002585613766")
 
     # other configs
     BOT_UPTIME  = time.time()
